70440d5e7d5e0f4223b578738bd48ca4d6e8efad
jastinesanchez <72461329+jastinesanchez@users.noreply.github.com>
2021-03-09 09:29:35 -0800

    Add files via upload

--- api.php
+++ api.php
@@ -28,19 +28,20 @@ if(isset($_REQUEST['cat']))
 {//check to be sure data has been transmitted via GET or POST
 	switch($_REQUEST['cat'])
 	{//determine contents of 'cat'
-		case "box":
+		/*case "box":
 			include('data/bond-box-office.js'); //"box" orders by box office results
-			break;
+			break; */
 		case "members":
 			include('data/Seventeen-members.js'); //this is for seventeen results
 			break;
 
-		case "units":
+		default: "units":
 			include('data/Seventeen-units.js'); //this is for seventeen results
-			break;
+		
 
-		default:
+		/*default:
 			include('data/bond-year.js'); //default orders by year
+			*/
 	}
 }else{//if not data sent, inform calling application
 	echo "Incorrect parameter sent";
